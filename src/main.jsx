ca511af389f9db1952868effb862667ae67e2917
Phimanao Paul <142014758+vilat23@users.noreply.github.com>
2024-03-13 00:37:06 +0100

    clean main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-// import Header from './components/header/Header.jsx'
-// import Hero from './components/hero/Hero.jsx'
 import './main.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
