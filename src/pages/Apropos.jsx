2d3a0ce8159b139c2ac9e98775a1ebb7d82f3860
Phimanao Paul <142014758+vilat23@users.noreply.github.com>
2024-03-20 04:15:59 +0100

    save

--- src/pages/Apropos.jsx
+++ src/pages/Apropos.jsx
@@ -1,8 +1,33 @@
-
+import Collapse from '../components/collapse/Collapse.jsx';
 function Apropos() {
   return (
     <div className="apropos">
-      <h1>Hello je suis la page A propos</h1>
+      <img src="../../src/assets/images/aproposBanner.jpg" alt="" className="apropos__img" />
+
+      <div className='apropos__collapses'>
+            <Collapse 
+            name ="Fiabilité" 
+            description="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont
+            régulièrement vérifiées par nos équipes."
+            />
+            <Collapse 
+            name ="Respect"
+            description="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de
+            perturbation du voisinage entraînera une exclusion de notre plateforme.."
+            />
+            <Collapse 
+            name ="Service"
+            description="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de
+            perturbation du voisinage entraînera une exclusion de notre plateforme."
+            />
+            <Collapse 
+            name ="Sécurité"
+            description="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement
+            correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au
+            locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons
+            également des ateliers sur la sécurité domestique pour nos hôtes."
+            />
+        </div>
 
     </div>
   );
